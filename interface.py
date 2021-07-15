1a97b5e01e9fb9318c57c44c4ef556094ad85b5f
Adrian Lundell <36153706+AdrianLundell@users.noreply.github.com>
2021-07-15 18:33:47 +0200

    Initial files

---
+++ interface.py
@@ -0,0 +1,239 @@
+import tkinter as tk 
+import tkinter.filedialog
+import tkinter.ttk as ttk 
+import os 
+
+from logic import * 
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+import matplotlib.pyplot as plt
+
+class MainWindow(tk.Tk):
+    """Main application class for the helmert transfrom interface"""
+
+    file_formats : list 
+    df_from : pd.DataFrame
+    df_to : pd.DataFrame
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.file_formats = [".sta", ".ssc"]
+        self.df_from = None
+        self.df_to = None
+
+        self.rotation_unit = tk.StringVar(self, value="si")
+        self.rotation_unit.trace("w", self.update_parameter_display)
+        self.translation_unit = tk.StringVar(self, value = "mm")
+        self.translation_unit.trace("w", self.update_parameter_display)
+
+        self.epoch = tk.StringVar(self)
+        self.epoch.trace("w", self.new_helmert_transform)
+        self.epoch.trace("w", self.df_from_change)
+        self.epoch.trace("w", self.df_to_change)
+
+        self.to_file_path = tk.StringVar(self)
+        self.to_file_path.trace("w", self.new_helmert_transform)
+        self.to_file_path.trace("w", self.df_to_change)
+        
+        self.from_file_path = tk.StringVar(self)
+        self.from_file_path.trace("w", self.new_helmert_transform)
+        self.from_file_path.trace("w", self.df_from_change)
+        
+        self.weighted = tk.BooleanVar(self, False)
+        self.weighted.trace("w", self.weighted_change)
+        self.transform_type = tk.StringVar(self, '7')
+        self.transform_type.trace("w", self.transform_type_change)
+
+        self.results_frame = tk.Frame(self)
+        self.plot = Plot(self.results_frame, 2, 1)
+        self.parameter_display = ParameterDisplay(self.results_frame)
+
+        self.control_frame = tk.Frame(self)
+        self.from_file_selecter_label = ttk.Label(self.control_frame, text="Transform from")
+        self.from_file_selecter = FileSelecter(self.control_frame, self.from_file_path, self.file_formats)
+        self.to_file_selecter_label = ttk.Label(self.control_frame, text="Transform to")
+        self.to_file_selecter = FileSelecter(self.control_frame, self.to_file_path, self.file_formats)
+        
+        self.weighted_button_label = ttk.Label(self.control_frame, text="Weighted")
+        self.weighted_button = ttk.Checkbutton(self.control_frame, variable=self.weighted, onvalue=True, offvalue=False)
+        self.transform_type_combo_label = ttk.Label(self.control_frame, text="N parameters")
+        self.transform_type_combo = ttk.Combobox(self.control_frame, textvariable=self.transform_type, values = ['7', '8', '9'])
+
+        self.epoch_entry_label = ttk.Label(self.control_frame, text="Epoch")
+        self.epoch_entry = ttk.Entry(self.control_frame, textvariable=self.epoch)
+
+        self.rotation_check_label = ttk.Label(self.control_frame, text="Rotation unit [classic/si]")
+        self.rotation_check = ttk.Checkbutton(self.control_frame, variable=self.rotation_unit, onvalue="sec", offvalue="si")
+        self.translation_check_label = ttk.Label(self.control_frame, text="Scale unit [cm/mm]")
+        self.translation_check = ttk.Checkbutton(self.control_frame, variable=self.translation_unit, onvalue="cm", offvalue="mm")
+
+        self.place_elements()
+
+    def place_elements(self):
+        """Geometry management"""
+
+        self.plot.grid(row=0, column=0, sticky="NESW")
+        self.parameter_display.grid(row=0, column=1, sticky="NESW", pady=(0,40))
+        self.results_frame.columnconfigure(0, weight=1)
+        self.results_frame.columnconfigure(1, weight=2)
+        self.results_frame.rowconfigure(0, weight=1)
+        self.results_frame.pack(expand=True, side="top", fill="both")
+
+        self.to_file_selecter_label.grid(row=0, column=0, sticky = "EW")
+        self.to_file_selecter.grid(row=0, column=1, sticky="EW")
+        self.from_file_selecter_label.grid(row=1, column=0, sticky = "EW")
+        self.from_file_selecter.grid(row=1, column=1, sticky="EW")
+        
+        self.weighted_button_label.grid(row=0, column=2)
+        self.weighted_button.grid(row=1, column=2, sticky= "EW")
+        self.transform_type_combo_label.grid(row=0, column=3)
+        self.transform_type_combo.grid(row=1, column=3, sticky= "EW")
+        self.epoch_entry_label.grid(row=0, column=4)
+        self.epoch_entry.grid(row=1, column=4)
+        
+        self.rotation_check_label.grid(row=0, column=5)
+        self.rotation_check.grid(row=1, column=5)
+        self.translation_check_label.grid(row=0, column=6)
+        self.translation_check.grid(row=1, column=6)
+        
+        self.control_frame.pack(expand=False, side="bottom", pady=40)
+        
+
+    def df_from_change(self, *args):
+        """Called on from_file change. Updates the df_from and calls new_helmert_transform"""
+        if not self.from_file_path.get()=="":
+            self.df_from = load_sta(self.from_file_path.get(), self.epoch.get())
+
+    def df_to_change(self, *args):
+        """Called on to_file change. Updates the df_from and calls new_helmert_transform"""
+        if not self.to_file_path.get()=="":
+            self.df_to = load_sta(self.to_file_path.get(), self.epoch.get())
+
+    def new_helmert_transform(self, *args):
+        """Runs a new helmert transform and updates the interface if both df_to and df_from are loaded"""
+
+        if not self.to_file_path.get() == "" and not self.from_file_path.get() == "":
+            self.transform = HelmertTransform(self.df_from, self.df_to, self.weighted.get(), self.transform_type.get())
+        
+            self.plot.clear()
+            self.transform.plot_residuals(self.plot.axes[0], self.plot.axes[1])
+            self.plot.draw()
+
+            self.update_parameter_display()
+
+    def update_parameter_display(self, *args):
+        """Updates the parameter display"""
+        if not self.to_file_path.get() == "" and not self.from_file_path.get() == "":
+            self.parameter_display.set(self.transform.repr(self.rotation_unit.get(), self.translation_unit.get()))
+
+    def weighted_change(self, *args):
+        """Called on weighted change"""
+        self.new_helmert_transform()
+
+    def transform_type_change(self, *args):
+        """Called on transform_type change"""
+        self.new_helmert_transform()
+
+class FileSelecter(tk.Frame):
+    """Custom tkinter widget for selecting a file through txt entry or a dialog button with validation"""
+
+    file_formats : list
+    external_var : tk.StringVar
+    internal_var : tk.StringVar
+    file_entry : ttk.Entry
+    file_button : ttk.Button
+
+    def __init__(self, master, external_var, file_formats, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+
+        self.file_formats = file_formats
+        self.external_var = external_var
+        self.internal_var = tk.StringVar(self)
+
+        self.file_entry = ttk.Entry(self, textvariable = self.internal_var)
+        self.file_entry.bind("<FocusOut>", self.file_change, add = "+")
+        self.file_entry.bind("<Tab>", self.file_change, add = "+")
+        self.file_entry.bind("<Return>", self.file_change, add = "+")
+
+        self.file_button = ttk.Button(self, command = self.file_dialog, text="Open")
+        self.place_elements()
+
+    def place_elements(self):
+        """Geometry management"""
+        self.file_entry.grid(row=0, column=0, sticky="EW")
+        self.file_button.grid(row=0, column=1, sticky="W")
+        
+        self.columnconfigure(0, weight=1)
+
+    def file_dialog(self, *args):
+        """Opens a file dialog window and updates the interval value"""
+        new_file_path = tk.filedialog.askopenfilename(title="Select EOP-file", 
+                                            filetypes=[("TRF-files", self.file_formats)])
+                                            #initialdir = self.intital_dir)
+        self.internal_var.set(new_file_path)
+        self.file_change()
+    
+    def file_change(self, *args):
+        """Updates the external var if the current path is valid, else reverts the internal var"""
+        new_file_path = self.internal_var.get() 
+        
+        if self.validate(new_file_path):
+            self.external_var.set(new_file_path)
+        else:
+            old_file_path = self.external_var.get()
+            self.internal_var.set(old_file_path)
+
+    def validate(self, file_path):
+        """Checks that the file path is valid and is not already selected"""
+        existing_path = os.path.exists(file_path)
+        same_path = (file_path == self.external_var.get())
+        
+        name, format = os.path.splitext(file_path)
+        correct_format = (format.lower() in self.file_formats)
+        
+        return not same_path and existing_path and correct_format
+
+class Plot(tk.Frame):
+    """Custom tkinter widget for using matplotlib with the interface"""
+
+    def __init__(self, master, subplot_rows, subplot_cols, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+
+        self.fig, self.axes = plt.subplots(subplot_rows, subplot_cols, figsize=(7,7))
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)    
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self)
+        
+        self.toolbar.update()
+        self.canvas.get_tk_widget().pack(expand=True, fill='both')
+
+    def clear(self):
+        """Clear all axes"""
+        if isinstance(self.axes, np.ndarray):
+            for ax in self.axes:
+                ax.clear()
+        else:      
+            self.axes.clear()
+
+    def draw(self):
+        """Show plot"""
+        self.canvas.draw()
+
+class ParameterDisplay(tk.Frame):
+    """Custom tkinter widget for easy display of text"""
+
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+
+        self.text = tk.Text(self, state = "disabled")
+        self.text.pack(expand=True, fill='both')
+
+    def set(self, string : str):
+        """Set text to string"""
+        self.text.config(state = "normal")
+        self.text.delete('1.0', tk.END)
+        self.text.insert('1.0', string)
+        self.text.config(state = "disabled")
+
+main = MainWindow()
+main.mainloop()
---
+++ logic.py
@@ -0,0 +1,298 @@
+from datetime import date
+from operator import pos
+from re import M
+import pandas as pd
+import geopandas as gpd
+import numpy as np 
+import matplotlib.pyplot as plt
+
+#CLASSES----------------------------------------------------------------------------------------------------------------
+class HelmertTransform():
+    """Wraps all analysis and visulisation of a helmert transform from df_from to df_to of the given type in a single object"""
+
+    weighted : bool                 #True for weighted least squares regression 
+    type : str                      #Type of helmert transform: 7, 8, or 9 parameters ()
+    parameters : dict               #Calculated helmert parameters
+    df_to : pd.DataFrame            #Values to transform from
+    df_from : pd.DataFrame          #Values to transfrom to
+    df_transformed : pd.DataFrame   #Transformed values
+    epoch : float                   #Epoch for value in modified julian date
+
+    def __init__(self, df_from: pd.DataFrame, df_to: pd.DataFrame, weighted: bool = False, type: str = "7", epoch = 0):
+        self.weighted = weighted 
+        self.type = type
+        self.epoch = epoch
+    
+        self.df_to = df_to
+        self.df_from = df_from
+        self.df_from = calculate_residuals(df_from, df_to) 
+        self.df_from = decompose_residuals(df_from)
+
+        self.parameters, self.sigmas = calculate_parameters(df_from, df_to, weighted, type)
+        self.df_transformed = helmert_transform(df_from, self.parameters)
+        
+        self.df_transformed = calculate_residuals(self.df_transformed, df_to) 
+        self.df_transformed = decompose_residuals(self.df_transformed)
+
+    def repr(self, unit, scale):
+
+        params = scale_parameters(self.parameters, unit, scale)
+        sigmas = scale_parameters(self.sigmas, unit, scale)
+
+        return f"""
+==========HELMERT TRANSFORMATION PARAMETERS======================
+           X                    Y                    Z 
+C       = {params["C"].T}
+S       = {params["S"].T}
+ω       = {params["OMEGA"].T}
+
+SIGMAS
+C_sigma = {sigmas["C"].T}
+S_sigma = {sigmas["S"].T}
+ω_sigma = {sigmas["OMEGA"].T}
+================================================================="""
+
+    def plot_sites(self, ax: plt.Axes):
+        plot_sites(self.df_from, ax)
+        plot_sites(self.df_to, ax)
+        plot_sites(self.df_transformed, ax)
+
+    def plot_residuals(self, ax1: plt.Axes, ax2: plt.Axes):
+        plot_residuals(self.df_to, self.df_from, self.df_transformed, ax1, ax2)
+
+    def plot_residuals_hist(self, ax1, ax2):
+        plot_residuals_hist(self.df_from, self.df_transformed, ax1, ax2)
+
+#HELMERT TRANSFORMATION FUNCTIONS--------------------------------------------------------------------------------------------
+def calculate_parameters(df_from: pd.DataFrame, df_to: pd.DataFrame, weighted: bool, type: str):
+    """Calculates Helmert parameters with a single scale factor using least squares, weighted or ordinary"""
+    
+
+    return parameters, sigmas
+
+def get_obeservation_matrix(df_from: pd.DataFrame, df_to: pd.DataFrame):
+    """Creates an observation matrix for an OLS parameter fitting of a Helmert-transform"""
+    observation_matrix = np.hstack((df_to.X-df_from.X, df_to.Y-df_from.Y, df_to.Z-df_from.Z))
+    return observation_matrix
+    
+def get_design_matrix(df: pd.DataFrame, type: str):
+    """Creates a design matrix for an OLS parameter fitting of a Helmer transform with seven, eight, or nine parameters"""
+    n = len(df.index)
+    zero = np.zeros(n)
+    one = np.ones(n)
+
+    if type == "7":
+        design_matrix_x = np.vstack((one, zero, zero, df.X, zero, df.Z, -df.Y))
+        design_matrix_y = np.vstack((zero, one, zero, df.Y, -df.Z, zero, df.X))
+        design_matrix_z = np.vstack((zero, zero, one, df.Z, df.Y, -df.X, zero))
+    elif type == "8":
+        design_matrix_x = np.vstack((one, zero, zero, df.X, zero, zero, df.Z, -df.Y))
+        design_matrix_y = np.vstack((zero, one, zero, df.Y, zero, -df.Z, zero, df.X))
+        design_matrix_z = np.vstack((zero, zero, one, zero, df.Z, df.Y, -df.X, zero))
+    elif type == "9":
+        design_matrix_x = np.vstack((one, zero, zero, df.X, zero, zero, zero, df.Z, -df.Y))
+        design_matrix_y = np.vstack((zero, one, zero, zero, df.Y, zero, -df.Z, zero, df.X))
+        design_matrix_z = np.vstack((zero, zero, one, zero, zero, df.Z, df.Y, -df.X, zero))
+    else:
+        raise Exception("Unrecoginzed type")     
+
+    design_matrix = np.hstack((design_matrix_x, design_matrix_y, design_matrix_z)).T
+    return design_matrix
+
+def get_weight_matrix(df_from: pd.DataFrame, df_to: pd.DataFrame):
+    """Creates a weight matrix for a WLS parameter fitting of a Helmer transform""" 
+    sigma = df_from.X**2 + df_from.Y**2 + df_from.Z**2 + df_to.X**2 + df_to.Y**2 + df_to.Z**2 
+    sigma = sigma.to_numpy()
+    weight_matrix = np.diag(np.tile(1/np.sqrt(sigma), 3))
+    
+    return weight_matrix
+
+def scale_parameters(parameters, rotation_unit, translation_unit):
+    
+    scaled_parameters = {}
+    if translation_unit == "cm":
+        scaled_parameters["C"] = parameters["C"]*100 #cm
+    elif translation_unit == "mm":
+        scaled_parameters["C"] = parameters["C"]*1000 #mm
+    
+    scaled_parameters["S"] = parameters["S"]*10**9 ##ppb
+
+    if rotation_unit == "sec":
+        arcseconds_per_radian =  206264.806
+        timeseconds_per_radian = 13750.9871
+        scaled_parameters["OMEGA"] = np.zeros((3,1))
+        scaled_parameters["OMEGA"][0:2] = parameters["OMEGA"][0:2] * arcseconds_per_radian * 10**3 #mas
+        scaled_parameters["OMEGA"][2] = parameters["OMEGA"][2] * timeseconds_per_radian * 10**6 #uas
+    elif rotation_unit == "si":
+        scaled_parameters["OMEGA"] = parameters["OMEGA"] * 10**9
+        
+    return scaled_parameters
+     
+
+def helmert_transform_full_form(df: pd.DataFrame, parameters: dict):
+    """Returns a new dataframe with transformed coordinates"""
+    coords = parameters["C"] + (1+parameters["S"]) * parameters["R"] @ np.vstack((df.X, df.Y, df.Z)) 
+    transformed_df = pd.DataFrame({"X": coords[0,:], 
+                                   "Y" : coords[1,:], 
+                                   "Z" : coords[2,:], 
+                                   "Station_Name" : df.Station_Name,
+                                   "LAT" : df.LAT,
+                                   "LONG" : df.LONG})
+
+    return transformed_df
+
+def helmert_transform(df: pd.DataFrame, parameters: dict):
+    """Returns a new dataframe with coordinates transformed according tp the helmert infinitecimal form"""
+    X = np.vstack((df.X, df.Y, df.Z)) 
+    coords = parameters["C"] + parameters["S"] * X + np.cross(parameters["OMEGA"], X, axis=0) + X
+    transformed_df = pd.DataFrame({"X": coords[0,:], 
+                                   "Y" : coords[1,:], 
+                                   "Z" : coords[2,:], 
+                                   "Station_Name" : df.Station_Name,
+                                   "LAT" : df.LAT,
+                                   "LONG" : df.LONG})
+
+    return transformed_df
+
+#RESIDUAL HANDLING FUNCTIONS-------------------------------------------------------------------------------------------------
+def calculate_residuals(df_from: pd.DataFrame, df_to: pd.DataFrame):
+    """Returns the from dataframe with computed residuals"""
+    df_from["dX"] = df_from.X-df_to.X
+    df_from["dY"] = df_from.Y-df_to.Y
+    df_from["dZ"] = df_from.Z-df_to.Z
+
+    return df_from
+
+def decompose_residuals(df):
+    """Returns the dataframe with residuals decomposed into U,E,N coordinates"""
+    df["dU"] = np.cos(df.LAT)*np.cos(df.LONG)*df.dX + \
+                np.cos(df.LAT)*np.sin(df.LONG)*df.dY + \
+                np.sin(df.LAT)*df.dZ
+    df["dE"] = np.sin(df.LONG)*df.dX + \
+                -np.cos(df.LONG)*df.dY + \
+            0
+    df["dN"] = np.sin(df.LAT)*np.cos(df.LONG)*df.dX + \
+               np.sin(df.LAT)*np.sin(df.LONG)*df.dY + \
+               (-np.cos(df.LAT)*np.cos(df.LONG)*np.cos(df.LONG)-np.cos(df.LAT)*np.sin(df.LONG)*np.sin(df.LONG))*df.dZ
+    
+    return df
+
+
+#PLOTTING FUNCTIONS-------------------------------------------------------------------------------------------------
+worldmap = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+
+def plot_sites3D(df: pd.DataFrame):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter3D(df.X, df.Y, df.Z)
+    plt.show()
+
+def plot_sites(df: pd.DataFrame, ax: plt.Axes):
+    """Plots the station sites scattered over a world map to ax"""
+    worldmap.plot(color="lightgrey", ax=ax)
+    ax.scatter(df.LAT, df.LONG, s=4)
+    #for x,y,s in zip(df.LAT, df.LONG, df.Station_Name):
+    #    plt.text(x,y,s, fontsize=5)
+    ax.grid()
+    
+def plot_residuals(df_to, df_from, df_transformed, ax1, ax2):
+    """Plots UEN-residuals scattered over a world map to axes"""
+    worldmap.plot(color="lightgrey", ax=ax1)
+    #ax1.quiver(df_from.LAT, df_from.LONG, df_from.dE, df_from.dN, color="b")
+    q = ax1.quiver(df_transformed.LAT, df_transformed.LONG, df_transformed.dE, df_transformed.dN, color="k", scale=2)
+    ax1.quiverkey(q, 0.9,1.05,2*10**-1, "2e-1 m", color = "red")
+    ax1.set_title("NE-residual components")
+    ax1.grid()
+
+    worldmap.plot(color="lightgrey", ax=ax2)
+    q = ax2.quiver(df_transformed.LAT, df_transformed.LONG, np.zeros(len(df_from.dU)), df_transformed.dU, scale=2)
+    ax2.set_title("UP-residual component")
+    ax2.quiverkey(q, 0.9,1.05,2*10**-1, "2e-1 m", color = "red")
+    ax2.grid()
+
+def plot_residuals_hist(df_from, df_transformed, ax1, ax2):
+    """Plots UEN-residuals plotted over value"""
+    ax1.hist([df_from.dU, df_from.dE, df_from.dN], 200, stacked=True)
+    ax2.hist([df_transformed.dU, df_transformed.dE, df_transformed.dN], 200, stacked=True)
+
+
+#DATA LOADING FUNCTIONS-------------------------------------------------------------------------------------------------
+def load_sta(fpath: str, epoch: float):
+    """Load a .sta TRF file to a pandas dataframe"""
+
+    column_names = ["Value_Type", "Station_Name", "Date", "X", "X_sigma", "Y", "Y_sigma", "Z", "Z_sigma", ]
+    column_specs = [(0,7), (10, 19), (19,25), (31,45), (52, 59), (65,79), (87,93), (99,113), (120,127)]
+    
+    df = pd.read_fwf(fpath, colspecs=column_specs, names = column_names)
+
+    df = df[df["Value_Type"]=="STA_GCX"]
+    df["Station_Name"] = df["Station_Name"].str.replace("_", "")
+    df["Date"] = pd.to_datetime(df["Date"], format = "%y%m%d")
+    df["Date"] = timestamp_to_year(df["Date"])
+    df = df.reset_index(drop=True)
+    df[["X", "Y", "Z"]] = df[["X", "Y", "Z"]].astype(np.float64)*10**-3
+    df[["X_sigma", "Y_sigma", "Z_sigma"]] = df[["X_sigma", "Y_sigma", "Z_sigma"]].astype(np.float64)*10**-3
+
+    df = df.drop(columns="Value_Type")
+    
+    df = calculate_long_lat(df)
+    return df
+
+#Save to csv.
+def load_ssc(fpath: str):
+    """Load a .ssc TRF file to a pandas dataframe"""
+    
+    column_names = ["Station_Name", "X", "Y", "Z", "X_sigma", "Y_sigma", "Z_sigma"]
+    column_specs = [(10, 26), (37,51), (51, 65), (65,78), (79,85), (86,92), (93,99)]
+    
+    df = pd.read_fwf(fpath, colspecs=column_specs, names = column_names, skiprows=7)
+    df = df.dropna()    
+    df = df.reset_index(drop=True)
+    df[["X", "X_sigma", "Y", "Y_sigma", "Z", "Z_sigma"]] = df[["X", "X_sigma", "Y", "Y_sigma", "Z", "Z_sigma"]].astype(np.float64)
+
+    df = calculate_long_lat(df)
+    return df
+
+def calculate_long_lat(df: pd.DataFrame):
+    """Add columns with LONG/LAT cooridnates to df derived from XYZ coordinates from df"""
+
+    df["LONG"] = np.degrees(np.arcsin(df.Z/(6371*10**3)))
+    df["LAT"] = np.degrees(np.arctan2(df.Y, df.X))
+
+    return df
+
+def timestamp_to_year(timestamp):
+    jd = pd.DatetimeIndex(timestamp).to_julian_date()
+    jd2000 = pd.Timestamp(year=2000, month=1, day=1).to_julian_date()
+    year = (jd - jd2000)/365.25 + 2000
+
+    return year
+
+"""New ideas
+DONE Units: 
+    Rotation 
+        Either nano-radians OR (micro arc second, mas, micro time seconds (differ by a factor of 24))
+
+        mas: 360 deg, 60 sec
+        Tme 360 deg 24 h 60min 60 sec 
+        Check box
+
+    Scale 
+        Parts perbillion
+
+    Translation 
+        millimeters / centimeters checkbox
+
+    Vector for scale in plot
+
+Reading files:
+    read in date, .sta : regex on name -> mjd 
+    .ssc: last column
+
+Put on: Given VLBI XYZ file: Plot up UEN values with ellipes
+    plot vectors and unc. 
+    put ellips at tip of vector
+    this is by memo 
+
+
+"""
